281ce8bea3f2a4396d4d0f421cccaf33c91e8f45
Legana Fingerhut <leganafingerhut@Leganas-iMac.local>
2019-03-19 15:12:57 +1000

    more model playtime

---
+++ R/predict_AMPs.R
@@ -0,0 +1,9 @@
+#this function calls the predict() function from caret package which is a wrapper for the e1071 package.
+#Should I rewrite the code to use the e1071 package instead to build the model?
+
+predict_AMPs <- function(df) {
+  #load model
+  load("svm_radial_model90p.Rdata")
+
+
+}
