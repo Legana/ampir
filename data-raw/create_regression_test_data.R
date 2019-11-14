46c686e0c21e2d8adf17184bf67345669614d826
Legana Fingerhut <legana.fingerhut@my.jcu.edu.au>
2019-11-14 18:08:44 +1000

    commented out saveRDS code to prevent overwriting

--- data-raw/create_regression_test_data.R
+++ data-raw/create_regression_test_data.R
@@ -12,10 +12,10 @@ little_seqs_features <- ampir:::calculate_features(little_seqs)
 
 hepseq <- "MALTVRIQAACLLLLLLASLTSYSLLLSQTTQLADLQTQDTAGATAGLMPGLQRRRRRDTHFPICIFCCGCCYPSKCGICCKT"
 hepcidin_paac <- ampir:::calc_pseudo_comp(hepseq, lambda_max = 4)
-saveRDS(hepcidin_paac,"tests/hepcidin_paac_lambda4.rds")
+#saveRDS(hepcidin_paac,"tests/hepcidin_paac_lambda4.rds")
 
 hepcidin_paac <- ampir:::calc_pseudo_comp(hepseq)
-saveRDS(hepcidin_paac,"tests/hepcidin_paac.rds")
+#saveRDS(hepcidin_paac,"tests/hepcidin_paac.rds")
 
 
 hepseq_pieces <- c(hepseq,substring(hepseq,1,11), substring(hepseq,1,8))
