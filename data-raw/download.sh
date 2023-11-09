cbe694a1a9bc54fc535b3c9629ea172bfaa71a10
Ira Cooke <iracooke@gmail.com>
2023-11-09 10:12:28 +1000

    Update data download urls

--- data-raw/download.sh
+++ data-raw/download.sh
@@ -1,2 +1,2 @@
-wget 'https://cloudstor.aarnet.edu.au/plus/s/cHIvI5Os5XvRu7V/download' -O tuned_precursor_imbal.rds
-wget 'https://cloudstor.aarnet.edu.au/plus/s/HM7e98FB6Ccpz4J/download' -O tuned_mature_final.rds
+wget 'http://data.qld.edu.au/public/Q5999/Legana/ampir/tuned_precursor_imbal.rds' -O tuned_precursor_imbal.rds
+wget 'http://data.qld.edu.au/public/Q5999/Legana/ampir/tuned_mature_final.rds' -O tuned_mature_final.rds
